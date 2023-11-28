199d73a4f668ca6b7c2a3faae585702abda67dbc
Ryota <disneyresort1983@icloud.com>
2023-11-28 22:57:19 +0900

    QuizRequestModelを修正

--- api/routers/admin/quiz_crud.py
+++ api/routers/admin/quiz_crud.py
@@ -22,26 +22,34 @@ async def get_quizzes(db: AsyncSession = Depends(get_db)) -> list[QuizResponseMo
         correct_answer: 正解,
     
     """
-    stmt = select(
-        QuestionOrm.id,
-        QuestionOrm.text,
-        ChoiceOrm.text.label("choice"),
-        QuestionOrm.correct_answer
-    ).join(ChoiceOrm, ChoiceOrm.question_id == QuestionOrm.id)
+    # レスポンス用の空配列を用意
+    all_quizzes = []
+
+    # 質問一覧を取得
+    stmt = select(QuestionOrm)
     result = await db.execute(stmt)
-    quizzes = result.all()
+    questions = result.scalars().all()
 
-    return [
-        QuizResponseModel(
-            question_id=quiz.id,
-            question_text=quiz.text,
-            correct_choice=quiz.choice,
-            wrong_choice=quiz.choice,
-            correct_answer=quiz.correct_answer
-        )
-        for quiz in quizzes
-    ]
+    # 取得した各質問に対する選択肢を取得
+    for question in questions:
+      stmt = select(ChoiceOrm).where(ChoiceOrm.question_id == question.id)
+      result = await db.execute(stmt)
+      choices = result.scalars().all()
 
+      # 取得した選択肢のうち1つ目を正解、2つ目を不正解とする。
+      correct_choice = choices[0].text
+      wrong_choice = choices[1].text
+
+      quiz = QuizResponseModel(
+            question_id=question.id,
+            question_text=question.text,
+            correct_choice=correct_choice,
+            wrong_choice=wrong_choice,
+            correct_answer=question.correct_answer
+      )
+      all_quizzes.append(quiz)
+    
+    return all_quizzes
 
 @router.post("/admin/create-quiz/")
 async def create_quiz(create_quiz: QuizRequestModel, db: AsyncSession = Depends(get_db)):
@@ -49,26 +57,34 @@ async def create_quiz(create_quiz: QuizRequestModel, db: AsyncSession = Depends(
 
       【Request】
         question_text: 質問文,
-        correct_answer: 正解,
-        choice_texts: 選択肢 = []
+        correct_choice: 正解選択肢,
+        wrong_choice: 不正解選択肢,
+        correct_answer: 正解
 
     """
+    # 質問文を保存
     create_question = QuestionOrm(
         text=create_quiz.question_text,
         correct_answer=create_quiz.correct_answer,
     )
     db.add(create_question)
-    await db.commit()
 
-    # コミット後にcreate_questionのidが取得できる
-    await db.refresh(create_question)
+    # のちにcreate_question.idを取得するため
+    await db.flush()
 
-    for choice_text in create_quiz.choice_texts:
-      create_choice = ChoiceOrm(
-          text=choice_text,
-          question_id=create_question.id
-      )
-      db.add(create_choice)
+    # 正解選択肢を保存
+    create_correct_choice = ChoiceOrm(
+        text=create_quiz.correct_choice,
+        question_id=create_question.id
+    )
+    db.add(create_correct_choice)
+
+    # 不正解選択肢を保存
+    create_wrong_choice = ChoiceOrm(
+        text=create_quiz.wrong_choice,
+        question_id=create_question.id
+    )
+    db.add(create_wrong_choice)
     await db.commit()
 
 
@@ -77,12 +93,13 @@ async def update_quiz(question_id: int, update_quiz: QuizRequestModel, db: Async
     """クイズを編集するエンドポイント
 
       【Request】
-        question_id: 質問ID,
         question_text: 質問文,
-        correct_answer: 正解,
-        choice_texts: 選択肢 = []
+        correct_choice: 正解選択肢,
+        wrong_choice: 不正解選択肢,
+        correct_answer: 正解
 
     """
+    # 質問文を更新
     update_question = (
        update(QuestionOrm)
        .where(QuestionOrm.id == question_id)
@@ -92,17 +109,16 @@ async def update_quiz(question_id: int, update_quiz: QuizRequestModel, db: Async
         )
     )
     await db.execute(update_question)
-    await db.commit()
 
-    for choice_text in update_quiz.choice_texts:
-      update_choice = (
-          update(ChoiceOrm)
-          .where(ChoiceOrm.question_id == question_id)
-          .values(
-            text=choice_text, 
-          )
-      )
-      await db.execute(update_choice)
+    # 指定の質問から2つの選択肢を取得
+    stmt = select(ChoiceOrm).where(ChoiceOrm.question_id == question_id)
+    result = await db.execute(stmt)
+    update_choices = result.scalars().all()
+
+    # 取得した2つの選択肢のうち1つ目を正解、2つ目を不正解の選択肢として更新
+    update_choices[0].text = update_quiz.correct_choice
+    update_choices[1].text = update_quiz.wrong_choice
+
     await db.commit()
 
 
--- api/schemas/request_model/quiz_crud.py
+++ api/schemas/request_model/quiz_crud.py
@@ -3,8 +3,9 @@ from typing import List
 
 class QuizRequestModel(BaseModel):
   question_text: str = Field(..., example="質問のテキスト")
+  correct_choice: str = Field(..., example="正解選択肢のテキスト")
+  wrong_choice: str = Field(..., example="不正解選択肢のテキスト")
   correct_answer: str = Field(..., example="正解のテキスト")
-  choice_texts: List[str]
-  
+
   class Config:
     orm_mode = True
