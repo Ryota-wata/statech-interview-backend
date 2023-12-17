ac1e1e74e6f8cf8be1d65ac54598fd36739472a0
mishina naoya <msnyxx811@gmail.com>
2023-12-17 19:52:17 +0900

    無駄な部分削除

--- api/routers/result.py
+++ api/routers/result.py
@@ -1,7 +1,6 @@
 import os
 import logging
 import firebase_admin
-import uuid
 from dotenv import load_dotenv
 from sqlalchemy import select,insert
 from fastapi import APIRouter, Depends, Request, HTTPException
@@ -15,42 +14,6 @@ from api.db import get_db
 
 
 router = APIRouter()
-
-# サービスアカウントキーの情報を環境変数から取得
-load_dotenv()
-
-type = os.environ.get("TYPE")
-project_id = os.environ.get("PROJECT_ID")
-private_key_id = os.environ.get("PRIVATE_KEY_ID")
-private_key = os.environ.get("PRIVATE_KEY")
-client_email = os.environ.get("CLIENT_EMAIL")
-client_id = os.environ.get("CLIENT_ID")
-auth_uri = os.environ.get("AUTH_URI")
-token_uri = os.environ.get("TOKEN_URI")
-auth_provider_x509_cert_url = os.environ.get("AUTH_PROVIDER_X509_CERT_URL")
-client_x509_cert_url = os.environ.get("CLIENT_X509_CERT_URL")
-universe_domain = os.environ.get("UNIVERSE_DOMAIN")
-
-# サービスアカウントキーの情報を辞書として作成
-service_account_info = {
-    "type": type,
-    "project_id": project_id,
-    "private_key_id": private_key_id,
-    "private_key": private_key,
-    "client_email": client_email,
-    "client_id": client_id,
-    "auth_uri": auth_uri,
-    "token_uri": token_uri,
-    "auth_provider_x509_cert_url": auth_provider_x509_cert_url,
-    "client_x509_cert_url": client_x509_cert_url,
-    "universe_domain": universe_domain
-}
-
-# # Firebase Admin SDKの初期化
-# cred = credentials.Certificate(service_account_info)
-# firebase_admin.initialize_app(cred)
-
-uuid = str(uuid.uuid4())
 
 #TODO 返り値の型指定
 @router.post("/result")
